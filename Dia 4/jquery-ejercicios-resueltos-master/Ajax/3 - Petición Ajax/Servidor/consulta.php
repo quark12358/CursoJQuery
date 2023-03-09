2dd0e0b9ae22e76d56d5e536023e0d4eb31bfe13
quark <ilorentequark@gmail.com>
2023-03-09 06:23:31 +0100

    44
    44

--- Dia 4/jquery-ejercicios-resueltos-master/Ajax/3 - Petición Ajax/Servidor/consulta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	echo "La fecha de nacimiento ". $_POST['fecha_nacimiento'].	" NO es válida";
-	echo "<br>";
-	echo "El código postal ". $_POST['codigo_postal']." SI es válido";
-	echo "<br>";
-	echo "El teléfono ".$_POST['telefono']." NO es válido";
-
-?>
